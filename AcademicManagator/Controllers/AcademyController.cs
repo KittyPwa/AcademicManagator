5a7d81e3b18fe8bedf02dc9f2518cf60b146f137
Unknown <c.leplumey@gmail.com>
2018-02-22 17:54:27 +0100

    (Feature) : Create Add

--- AcademicManagator/Controllers/AcademyController.cs
+++ AcademicManagator/Controllers/AcademyController.cs
@@ -21,7 +21,33 @@ namespace AcademicManagator.Controllers
                     Establishments = (ICollection<Establishments>)acad.Establishments
 
                 }).ToList<AcademyModel>();
-            return View("Create", result);
+            return View("Index",result);
+        }
+
+        public ActionResult Create()
+        {
+            return View("Create",new AcademyModel
+            {
+                Id = Guid.NewGuid()
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Create(AcademyModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            AcademyRepository sr = new AcademyRepository(new AcademyEntities());
+            sr.Add(new Academies
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Establishments = (ICollection<Establishments>)model.Establishments.AsEnumerable()
+            });
+            return null;
+            //return View("Index", ToList<model>); ;
         }
     }
 }
